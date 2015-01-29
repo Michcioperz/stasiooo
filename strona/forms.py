21a241999e955c71dea397ee93d605a1eb22741f
Michał Sidor <michcioperz@autistici.org>
2015-01-29 13:34:52 +0100

    trying to write some forms at school

---
+++ strona/forms.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+from django import forms
+from tinymce import widgets
+import datetime
+
+
+class OwnEntryForm(forms.Form):
+    title = forms.CharField(label="Tytuł", max_length=255)
+    content = forms.CharField(label="Treść", widget=TinyMCE(attrs={"cols": 80, "rows": 30}))
+    publication_date = forms.DateTimeField(label="Czas publikacji", initial=datetime.datetime.now())
