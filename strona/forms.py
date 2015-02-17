fc02d1e46c5ddec61d7fb20ed85da91e5c754e05
Michał Sidor <michcioperz@autistici.org>
2015-02-17 16:41:21 +0100

    Update forms.py

--- strona/forms.py
+++ strona/forms.py
@@ -8,6 +8,6 @@ class OwnEntryForm(forms.Form):
     title = forms.CharField(label="Tytuł", max_length=255)
     content = forms.CharField(label="Treść",
                               widget=widgets.TinyMCE(attrs={"cols": 80,
-                                                            "rows": 30}))
+                                                            "rows": 10}))
     publication_time = forms.DateTimeField(label="Czas publikacji",
                                            initial=datetime.datetime.now())
